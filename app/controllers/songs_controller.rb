5813bcd5f8002e61b88e7b61d74275323602ae70
dynamic-daemon-637b <zl3087@columbia.edu>
2021-07-05 00:50:48 +0000

    add

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,49 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
-      get '/songs' do
+  use Rack::Flash
+
+  get '/songs' do
     @songs = Song.all
-    erb:'songs/index'
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+
     erb :'songs/show'
   end
 
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect("/songs/#{@song.slug}")
+  end
+  
+  get '/songs/:id/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+   
+   patch 'songs/id' do
+  @song = Song.find_by_slug(params[:slug])
+  @song.update(params["song"])
+   if !params["artist"]["name"].empty?
+      @song.artist=Song.create(name: params["artist"]["name"])
+    end
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect ("/songs/#{@song.slug}")
+
+  end
+  
 end
